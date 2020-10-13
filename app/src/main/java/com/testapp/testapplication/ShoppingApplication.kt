c005f0347b2acd760f6feca4e956ac5c7d972da0
sina <cena.tabriziyan@gmail.com>
2020-10-13 20:24:03 +0330

    Implement Dagger Hilt for Room , Retrofit

---
+++ app/src/main/java/com/testapp/testapplication/ShoppingApplication.kt
@@ -0,0 +1,9 @@
+package com.testapp.testapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class ShoppingApplication: Application() {
+}
---
+++ app/src/main/java/com/testapp/testapplication/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.testapp.testapplication.di
+
+import android.content.Context
+import androidx.room.Room
+import com.testapp.testapplication.constants.Constnts.BASE_URL
+import com.testapp.testapplication.constants.Constnts.DATABASE_NAME
+import com.testapp.testapplication.data.local.ShoppingItemDatabase
+import com.testapp.testapplication.data.remote.PixabayApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providePixabayApi(): PixabayApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayApi::class.java)
+    }
+
+
+}
